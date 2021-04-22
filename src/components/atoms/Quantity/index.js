38cdf9e030179a67a631783d868ce96a3a167f91
Michal Janiec <michal.janiec95@gmail.com>
2021-04-23 01:01:19 +0200

    Added readOnly attribute to protect before directing data input

--- src/components/atoms/Quantity/index.js
+++ src/components/atoms/Quantity/index.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import editQtyIcon from 'assets/images/editQtyIcon.png';
 import { Wrapper, QuantityValue } from './styled';
 import { Button } from 'components/atoms/Button';
+import { decreaseQuantity, increaseQuantity } from 'views/productsSlice';
 
-const Quantity = ({ productQuantity }) => {
+const Quantity = ({ productQuantity, updateSubTotal, productId }) => {
+  const [quantity, setQuantity] = useState(productQuantity);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setQuantity(productQuantity);
+  }, [productQuantity]);
+
   return (
     <Wrapper>
-      <Button quantity>-</Button>
-      <QuantityValue type="number" defaultValue={productQuantity} />
-      <Button quantity>+</Button>
-      <Button itemAction>
+      <Button
+        quantity
+        onClick={() => dispatch(decreaseQuantity(productId))}
+        disabled={productQuantity === 0 ? true : false}
+      >
+        -
+      </Button>
+      <QuantityValue
+        readOnly
+        min={1}
+        type="number"
+        value={quantity}
+        onChange={({ target }) => setQuantity(target.value)}
+      />
+      <Button quantity onClick={() => dispatch(increaseQuantity(productId))}>
+        +
+      </Button>
+      <Button itemAction onClick={() => updateSubTotal()}>
         <img src={editQtyIcon} alt="productIcon" />
       </Button>
     </Wrapper>
--- src/components/atoms/Quantity/styled.js
+++ src/components/atoms/Quantity/styled.js
@@ -3,10 +3,19 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   display: flex;
   align-items: center;
+
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  input[type='number'] {
+    -moz-appearance: textfield;
+  }
 `;
 
 export const QuantityValue = styled.input`
-  -webkit-appearance: none;
   width: 60px;
   padding: 8px;
   border: 2px solid;
