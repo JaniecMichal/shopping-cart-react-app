ebab6035c49db3edd58f05c1fb7febedf3af3527
Michal Janiec <michal.janiec95@gmail.com>
2021-04-22 23:35:51 +0200

    Created local state for rendering ShippmentCost components and Totals Compoents. Used useEffect hook for updating value wen subTotal will change

--- src/views/App.js
+++ src/views/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { MainWrapper } from 'components/atoms/MainWrapper';
 import { MainHeader } from 'components/atoms/MainHeader';
 import { Button } from 'components/atoms/Button';
@@ -6,8 +7,21 @@ import { Section } from 'components/atoms/Section';
 import CostDetails from 'components/atoms/CostDetails';
 import Totals from 'components/molecules/Totals';
 import ProductsCart from 'components/organisms/ProductsCart';
+import { selectSubTotal } from './productsSlice';
 
 function App() {
+  const subTotal = useSelector(selectSubTotal);
+  const [shippingCost, setShippingCost] = useState(23.8);
+  const [grandTotal, setGrandTotal] = useState(subTotal + shippingCost);
+
+  useEffect(() => {
+    if (subTotal > 100) {
+      setShippingCost(0);
+      setGrandTotal(subTotal);
+    }
+    return;
+  }, [subTotal]);
+
   return (
     <MainWrapper>
       <Section>
@@ -17,8 +31,12 @@ function App() {
       <Section details>
         <ProductsCart />
         <Section costs>
-          <CostDetails shipping costName={'shipping'} costValue={'$23.80'} />
-          <Totals />
+          <CostDetails
+            shipping
+            costName={'shipping'}
+            costValue={shippingCost}
+          />
+          <Totals subTotal={subTotal} grandTotal={grandTotal} />
         </Section>
       </Section>
     </MainWrapper>
