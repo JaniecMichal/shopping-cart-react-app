a899b97bbd523c68fbd37b1ed7d40c84c27fe0fd
JaniecMichal <67117780+JaniecMichal@users.noreply.github.com>
2021-04-23 11:59:29 +0200

    changed method of importing fonts to remove warning in console

--- src/assets/GlobalStyles/GlobalStyles.js
+++ src/assets/GlobalStyles/GlobalStyles.js
@@ -2,7 +2,6 @@ import { createGlobalStyle } from 'styled-components';
 import { normalize } from 'styled-normalize';
 
 export const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap');
   ${normalize}
         html {
         box-sizing: border-box
