b224dfce88ad48aeb1b9aa2ff4d2157adb57db30
JaniecMichal <67117780+JaniecMichal@users.noreply.github.com>
2021-04-23 13:49:48 +0200

    Added component with confirmation communicat

---
+++ src/components/atoms/ConfirmationInfo/index.js
@@ -0,0 +1,13 @@
+import styled, { css } from 'styled-components';
+
+export const ConfirmationInfo = styled.h2`
+  color: ${({ theme }) => theme.colors.apple};
+  fontsize: ${({ theme }) => theme.fontSize.big};
+  display: none;
+
+  ${({ isConfirmed }) =>
+    isConfirmed &&
+    css`
+      display: inherit;
+    `}
+`;
